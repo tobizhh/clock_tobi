c6991f316140fcc70c5e8221b78b602d370c1ca8
tobizhh <grrtiprrti@gmail.com>
2024-11-20 11:37:28 +0100

    2

--- analogClock.js
+++ analogClock.js
@@ -1,15 +1,16 @@
-// script.js
-const hourHand = document.querySelector('.hour-hand');
+//explain purpose of each hand//
+const hourHand = document.querySelector('.hour-hand'); 
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
 
+//function to sync hands with time//
 function setDate() {
-    const now = new Date();
-
+    const now = new Date(); //current date and time//
+    //calculate seconds and their rotation in degres//
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const secondsDegrees = ((seconds / 60) * 360) + 90; //calculate to degrees and offset by 90 to align start position//
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; //rotation of second-hand//
     
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
@@ -20,6 +21,8 @@ function setDate() {
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
+//intervall to update every second//
 setInterval(setDate, 1000);
 
+//call function//
 setDate();
